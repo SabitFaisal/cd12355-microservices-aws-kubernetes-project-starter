e8b55dd997dc6f5a9f449e6c697de9c8d4b28520
SabitFaisal <sabitewu@gmail.com>
2024-07-29 16:43:18 +0000

    adding buildspec

---
+++ db/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U myuser -d mydatabase -p 5433
+# Define the command you want to run
+command_to_run="PGPASSWORD='$DB_PASSWORD' psql --host 127.0.0.1 -U myuser -d mydatabase -p 5433 < 1_create_tables.sql"
+
+# Execute the command
+echo "Executing command: $command_to_run"
+eval $command_to_run
+
+# Check the exit status
+if [ $? -eq 0 ]; then
+    echo "Command executed successfully."
+else
+    echo "Command failed with exit status $?."
+fi
